42e3a31e1ac2d0bb06aea776ce1580d7d6084577
Imam <imam2889@gmail.com>
2017-12-10 22:50:17 +0600

    there are lots of function

---
+++ stringformating.py
@@ -0,0 +1,20 @@
+msg = "There is my result in those subject C: {1}, Database: {2}, Datbase:{0}".format(4.0,3.75,3.30)
+print(msg)
+
+
+message = "If x = {x} and y = {y}, then x+y = {z}".format(x = 20, y = 300, z = 20+300)
+print(message)
+
+print(" ,".join(["apple", "orange", "pineapple"])) 
+
+print("Hello ME".replace("ME", "world"))
+
+print("This is a sentence.".startswith("This"))
+
+print("This is a sentence.".endswith("sentence."))
+
+print("This is a sentence.".upper())
+
+print("AN ALL CAPS SENTENCE".lower())
+
+print("a, e, i, o, u".split(", "))
