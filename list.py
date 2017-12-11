881c6caa204070b8907ec6520b087fca83d78439
Imam <imam2889@gmail.com>
2017-12-11 18:27:56 +0600

    here we saw different types of list

---
+++ list.py
@@ -0,0 +1,25 @@
+words = ["Hello", "world", "!"]
+print(words[0])
+print(words[1])
+print(words[2])
+
+
+#blank list
+
+my_list = []
+print("print blank list from here")
+print(my_list)
+
+#element type
+
+number = 1
+my_numbers = [number, 2, 3]
+
+things = ["Numbers", 0, my_numbers, 4.56]
+
+print("print element types from here")
+
+print(things[0])
+print(things[1])
+print(things[2])
+print(things[2][2])
