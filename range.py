dc47e6080d1dbf3a1b10de91c6510f5673e03560
Imam <imam2889@gmail.com>
2017-12-11 20:22:22 +0600

    working with range

---
+++ range.py
@@ -0,0 +1,18 @@
+my_range = list(range(10))
+print(my_range)
+
+#output [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print("print number between 5 to 10")
+
+my =list(range(5,10))
+print(my)
+
+#ouput [5, 6, 7, 8, 9]
+
+print("print number 5 to 30 and each difference is 3")
+
+my =list(range(5,30,3))
+print(my)
+
+#ouput [5, 8, 11, 14, 17, 20, 23, 26, 29]
