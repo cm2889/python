99a6a6f3864d6f83219ef9c5538c8c8a96c6ba6b
Imam <imam2889@gmail.com>
2017-12-11 17:42:42 +0600

    we see here some boliean logic

---
+++ boolieanlogic.py
@@ -0,0 +1,21 @@
+#and opeartion
+1 == 1 and 2 == 2
+True
+
+#or operation
+1 == 1 or 2 == 3
+True
+#not operation
+not 1 == 1
+False
+
+a = 1
+b = 1
+
+c = 5
+d = 10
+
+if a==b and c<d:
+    print("a and b is equal and c is smaller than d")
+else:
+    print("please check thois")
