404c65b3ae12c0924f4602203ae92cc2ddc487d1
Imam <imam2889@gmail.com>
2017-12-11 20:12:19 +0600

    discuss some list function

---
+++ listfunction.py
@@ -0,0 +1,26 @@
+# append.... কোন একটি লিস্টের শেষে নতুন এলিমেন্ট যুক্ত করতে এই মেথড ব্যবহার করা যাবে
+
+
+nums = [1, 2, 3]
+nums.append(4)
+print(nums)
+
+
+#insert add a number any place
+print("This is the task of insert function")
+words = ["A", "C"]
+index = 1
+words.insert(index, "B")
+
+print(words)
+
+#count লিস্টের মধ্যে কোন একটি এলিমেন্ট মোট কতবার আছে তার সংখ্যা জানতে
+
+print("we print how many p are present in this element")
+letters = ['p', 'q', 'r', 's', 'p', 'u']
+letters.count('p')
+
+#max() number
+print("find the maximum numbers")
+nums = [1, 2, 4, 20, 50, 3, 4]
+max(nums)
