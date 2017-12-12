748c9e0b179796d0cea7ededdf5dc85fc4e0d1e1
Imam <imam2889@gmail.com>
2017-12-12 12:05:15 +0600

    some dictionary function are avilable inthis chapter

---
+++ dicfunction.py
@@ -0,0 +1,10 @@
+my_list = [2, 4, 6, 7]
+my_list[3] = 8
+
+print(my_list)
+
+#using get method for unfacing error
+
+print("using get method")
+my_nums = {1 : 1, 2 : 4, 3 : 9, 4 : 16}
+print(my_nums.get(5, "5 not in my numbers!"))
