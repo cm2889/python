fe3315500bd4bc6eb368f7e66058ac4352e3e86c
Imam <imam2889@gmail.com>
2017-12-17 15:10:05 +0600

    convert binary,hexadecimal,octal

---
+++ miniproject/convertnumber.py
@@ -0,0 +1,11 @@
+# Python program to convert decimal number into binary, octal and hexadecimal number system
+
+dec = 2889
+
+print("The decimal value is : ",dec)
+
+print("The binary value is :",bin(dec))
+
+print("The octal number is :",oct(dec))
+
+print("hexadecimal number is :",hex(dec))
